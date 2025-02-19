5fd7a00cdf2abe7937e16f5b85d2cd3990b07eb3
darlenenyambura <darlene.nyambura@student.moringaschool.com>
2025-02-19 09:37:09 +0300

    eyes on me

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,10 +1,13 @@
 // Code Keypad Component Here
 
 function Keypad (){
+    function handleChange() { //test: event handler that listens to the change
+        console.log("Entering password...");
+    }
     return (
         <div>
-            <input type="password" />
-        </div>
+            <input type="password" onChange={handleChange} />;
+        </div> //test: rendering intput
     );
 }
 
